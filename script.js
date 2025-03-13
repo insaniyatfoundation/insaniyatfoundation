6242bf203ffdeaaa76dcc257e1c147f7af313e05
insaniyatfoundation <salmannpathann786@gmail.com>
2025-03-13 20:53:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-document.getElementById('donateBtn').addEventListener('click', function() {
-    document.getElementById('googleScanner').style.display = 'block';
-    window.scrollTo({
-        top: document.body.scrollHeight,
-        behavior: 'smooth'
-    });
-});
 
-document.getElementById('closeScanner').addEventListener('click', function() {
-    document.getElementById('googleScanner').style.display = 'none';
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
-});
-// JavaScript for any dynamic interactions, if required (for example, smooth scrolling)
