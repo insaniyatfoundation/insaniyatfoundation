d60c6f50a52f9a5e311314c65d5e095db197834f
insaniyatfoundation <salmannpathann786@gmail.com>
2025-03-13 20:56:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('donateBtn').addEventListener('click', function() {
+    document.getElementById('google-payment').scrollIntoView({ behavior: 'smooth' });
+    document.getElementById('google-payment').style.display = 'block';
+});
